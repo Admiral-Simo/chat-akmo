25d6b3515c3cc47dc4a7c607fffe2f331a07f0b2
Admiral-Simo <admiralsimo16@gmail.com>
2022-12-29 14:10:53 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+function Test() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-red-500'>First screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Test" component={Test} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
